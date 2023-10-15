861aab9261f6c01f28aead1f064b7eb0289922d6
afruzaakter <afruzaaktersonia1995@gmail.com>
2023-10-15 16:16:26 +0600

    car website code push

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,5 +1,40 @@
+'use client';
+import {useContext, useEffect, useState } from "react";
+//next image
+import Image from 'next/image';
+//react scroll 
+import {Link} from 'react-scroll';
 
-export default function Header(props) {
+//components
+import SearchMobile from "./SearchMobile";
+//media query hook
+import { useMediaQuery } from "react-responsive";
+export default function Header() {
+    const [header, setHeader] = useState(false);
+    const [nav, setNav] = useState(false);
+
+    const desktopMode = useMediaQuery({
+        query: '(min-width: 1300px)',
+    });
+
+    useEffect(()=>{
+        const handleScroll = () =>{
+            // header
+            if(window.scrollY > 40){
+                setHeader(true);
+            }else{
+                setHeader(false)
+            }
+        };
+
+        // add event listener
+        window.addEventListener('scroll', handleScroll);
+
+        // remove event listener
+        
+
+    })
+
     return (
         <div>
             Header
